42721cb9f2c77f8c317a794e65e2f845172672bc
Gary Rancier <gary@policyfocus.org>
2014-09-11 21:33:13 -0400

    The article title comes before the featured image.

--- wp-content/themes/padhang/content.php
+++ wp-content/themes/padhang/content.php
@@ -6,14 +6,15 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-
+                <?php //Article title is displayed above the featured image, and underneath the site logo ?>
+                <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 		<?php if ( has_post_thumbnail() ) : ?>
 			<div class="featured-image">
 				<?php the_post_thumbnail('full'); ?>
 			</div>
 		<?php endif; ?>
 
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+		
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
